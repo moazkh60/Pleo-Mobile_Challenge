b2be79b72069967142f9b40fe5c10514fc012daa
Muhammad Moaz Khan <moazkh60@gmail.com>
2019-10-19 10:22:56 +0500

    Created common stylesheet

---
+++ src/common/Stylesheet.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+
+/**
+ * Since the styles we need are common across
+ * all files so the styles are refactored into
+ * single file from separate files.
+ */
+export const styles = StyleSheet.create({
+    
+})
